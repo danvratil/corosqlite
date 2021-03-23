54eab927ed0c1ccf2bc0ae64ce73590eb46c96b5
Daniel Vrátil <me@dvratil.cz>
2021-03-23 15:51:26 +0100

    Attempt for clang and MSVC support

--- corosqlite/coro.h
+++ corosqlite/coro.h
@@ -1,4 +1,22 @@
+#if defined(__clang__)
+#include <experimental/coroutine>
+namespace corosqlite::coro {
+    using suspend_never = std::experimental::suspend_never;
+    using suspend_always = std::experimental::suspend_always;
+    template<typename T>
+    using coroutine_handle = std::experimental::coroutine_handle<T>;
+} // namespace corosqlite::coro
+#elif defined(__GNUC__) || defined(_MSC_VER)
 #include <coroutine>
+namespace corosqlite::coro {
+    using suspend_never = std::suspend_never;
+    using suspend_always = std::suspend_always;
+    template<typename T>
+    using coroutine_handle = std::coroutine_handle<T>;
+} // namespace corosqlite::coro
+#else
+#pragma error "Current compiler is not supported (yet)."
+#endif
 #include <cassert>
 #include <utility>
 #include <stdexcept>
@@ -11,18 +29,18 @@ public:
     struct promise_type {
         T result_;
 
-        using coro_handle = std::coroutine_handle<promise_type>;
+        using coro_handle = coro::coroutine_handle<promise_type>;
         auto get_return_object() {
             return coro_handle::from_promise(*this);
         }
 
-        constexpr std::suspend_never initial_suspend() { return {}; };
-        constexpr std::suspend_always final_suspend() { return {}; };
-        constexpr void return_void() {};
+        constexpr coro::suspend_never initial_suspend() noexcept { return {}; };
+        constexpr coro::suspend_always final_suspend() noexcept { return {}; };
+        constexpr void return_void() noexcept {};
 
         auto yield_value(T &&result) {
             result_ = std::move(result);
-            return std::suspend_always();
+            return coro::suspend_always();
         }
 
         auto unhandled_exception() {
@@ -30,7 +48,7 @@ public:
         }
     };
 
-    using coro_handle = std::coroutine_handle<promise_type>;
+    using coro_handle = coro::coroutine_handle<promise_type>;
 
     ResultGenerator(coro_handle handle)
         : handle_(handle) {};
